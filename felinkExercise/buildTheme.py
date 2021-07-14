03091309b15b40c33db23d42e6fc7d74b06fac0c
Weilin X <35093089+sisyphusLim@users.noreply.github.com>
2021-07-14 18:45:33 +0800

    1

--- felinkExercise/buildTheme.py
+++ felinkExercise/buildTheme.py
@@ -11,19 +11,33 @@ import time
 
 
 driver = connet_XT()
+driver.implicitly_wait(10)
 
-time.sleep(3)
+
 diy_tab = driver.find_element_by_id("main_tab_diy_layout")
 if diy_tab.is_selected() == False:
-    print("点击'定制'tab页")
+    print("回到首页")
     diy_tab.click()
 
-#time.sleep(3)
-driver.find_element_by_xpath("//*[@text='定制']").click()
-time.sleep(3)
-driver.find_element_by_xpath("//*[@text='特效模板']").click()
-time.sleep(3)
-driver.find_element_by_xpath("//*[@text='编号:3577565']").click()
+def search_theme(theme_id,driver=driver):
+    # 点击输入框，输入模板编号进行搜索
+    try:
+        #模糊搜索“搜索”
+        driver.find_element_by_android_uiautomator('new UiSelector().textContains("搜索")').click()
+        driver.find_element_by_id('search_navigation_edit_text').send_keys("{}".format(theme_id))
+        driver.find_element_by_id('search_navigation_search').click()
+        driver.find_element_by_xpath("//*[@text='编号:{}']".format(theme_id)).click()
+    except:
+        print("出错了")
+
+search_theme("3577565", driver)
+
+def make_theme(theme_id,driver=driver):
+    
+    search_theme(theme_id)
+    if 
+
+
 
 
 
