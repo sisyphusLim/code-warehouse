2c61537b6c8237dbc1f2ad437c3123f63c24f7ca
Weilin X <35093089+sisyphusLim@users.noreply.github.com>
2021-07-15 18:48:45 +0800

    1

--- felinkExercise/buildTheme.py
+++ felinkExercise/buildTheme.py
@@ -29,13 +29,39 @@ def search_theme(theme_id,driver=driver):
         driver.find_element_by_xpath("//*[@text='编号:{}']".format(theme_id)).click()
     except:
         print("出错了")
-
-search_theme("3577565", driver)
+#测试一下
+#search_theme("3577565", driver)
 
 def make_theme(theme_id,driver=driver):
     
     search_theme(theme_id)
-    if 
+    driver.find_element_by_id("tv_make").click()
+    #这里如果未登录，需要插入一个登录会员账号的流程。
+    driver.find_element_by_id("img_select_material").click()
+
+    #假使需要指定图片目录
+    #driver.find_element_by_id("mSetArrowImg").click()
+    #driver.find_element_by_android_uiautomator('resourceId("name").text("我的壁纸")').click()
+
+    #下面查找时使用了find_elements_by_xxxx，会报错'list' object has no attribute 'click' 因为找到的元素是一个列表
+    driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.felink.videopaper.mi:id/mRoot").index(14)').click()
+    
+    driver.find_element_by_id("tv_rightBtn").click()
+    driver.find_element_by_id("crop_finish_btn").click()
+    #可能出现图片无法识别的问题，需要引入纠错流程
+    driver.find_element_by_id("btn_next").click()
+    driver.find_element_by_id("tv_make").click()
+
+    #等待合成完成，保存并返回，预留30s
+    WebDriverWait(driver, 30).until(lambda x: x.find_element_by_id(("tv_save")).is_displayed())
+    driver.find_element_by_id("tv_save").click()
+    driver.find_element_by_id("img_close").click()
+    print("模板{}合成完成".format(theme_id))
+
+
+
+make_theme("3577565",driver)  
+    
 
 
 
